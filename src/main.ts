35d850271640275d8e50508bfd87e51fa8df11fe
Jonny200220 <jonnyrmzhdz@gmail.com>
2025-12-30 10:51:31 -0600

    feat: agregando modelo de datos para el usuario, y activando el cors para el puerto 3000

--- src/main.ts
+++ src/main.ts
@@ -5,6 +5,7 @@ async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   await app.listen(process.env.PORT ?? 3000);
 
+  app.enableCors({ origin: 'http://localhost:3000' });
   app.setGlobalPrefix('api/v1');
 }
 bootstrap();
